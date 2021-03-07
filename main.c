31ac56f25037df5be8eff18bcae909b6783a5bd6
DiegoNS <diego.nunes.xarq@gmail.com>
2021-03-07 17:41:16 +0000

    Exercício 00

--- main.c
+++ main.c
@@ -1,15 +1,5 @@
 /*#include <stdio.h>
-
 int main(void) {
-  int a,b;
-	printf("Digite um numero ");
-	scanf("%i", &a);
-	printf("Digite um numero ");
-	scanf("%i", &b);
-	if(a<b){
-		printf("%i %i",a,b);
-	}else{
-		printf("%i %i", b,a);
-	}
-  return 0;
+		printf("Hello Word");
+	  return 0;
 }*/
